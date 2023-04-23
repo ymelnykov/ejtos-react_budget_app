20345c44cd4341ca84c4c374625e1bb4f7e09753
Yuriy Melnykov <97179527+ymelnykov@users.noreply.github.com>
2023-04-23 21:16:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
+import Currency from './components/Currency';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import { AppProvider } from './context/AppContext';
@@ -24,6 +25,9 @@ const App = () => {
                         </div>
                         <div className='col-sm'>
                             <ExpenseTotal />
+                        </div>
+                        <div className='col-sm'>
+                            <Currency />
                         </div>
                     </div>
                     <h3 className='mt-3'>Allocation</h3>
