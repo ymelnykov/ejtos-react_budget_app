4667bebcaa9968371142d95e15d5c66c4756419d
Yuriy Melnykov <97179527+ymelnykov@users.noreply.github.com>
2023-04-23 21:05:55 +0300

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,32 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import ExpenseTotal from './ExpenseTotal';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, currency, dispatch } = useContext(AppContext);
+
+    const setBudget = (val) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload:val,
+        })
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
+            <span>Budget:   {currency}
+                <input
+                required='required'
+                max='20000'
+                min={ExpenseTotal}
+                step='10'
+                type='number'
+                id='budget'
+                value={budget}
+                style={{width: '8rem'}}
+                onChange = {e => setBudget(e.target.value)}>
+                </input>
+            </span>
+        </div >
     );
 };
 export default Budget;
+
